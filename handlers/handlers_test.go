56281c51d2744792ae078a114aa314a8b524eaea
Ilya <sov16@ya.ru>
2026-06-23 12:40:42 +0300

    add tests

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -14,8 +15,15 @@ import (
 func strPtr(s string) *string { return &s }
 
 func testRequest(t *testing.T, ts *httptest.Server, method string,
-	path string, contentType *string, body string) (*http.Response, string) {
-	url_body := strings.NewReader(body)
+	path string, contentType *string, body *string) (*http.Response, string) {
+	var url_body io.Reader
+	if body != nil {
+		url_body = strings.NewReader(*body)
+	}
+	client := ts.Client()
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
 	req, err := http.NewRequest(method, ts.URL+path, url_body)
 	require.NoError(t, err)
 	if contentType != nil {
@@ -49,7 +57,11 @@ func TestRouter(t *testing.T) {
 		{"/", strPtr("https://www.yandex.ru"), want{201, "text/plain"}, "POST"},
 	}
 	for _, v := range testTable {
-		resp, _ := testRequest(t, ts, v.method, v.url, &v.want.contentType, *v.body)
+		resp, shortUrl := testRequest(t, ts, v.method, v.url, &v.want.contentType, v.body)
 		assert.Equal(t, v.want.code, resp.StatusCode)
+		parsedURL, err := url.Parse(shortUrl)
+		require.NoError(t, err)
+		resp_get, _ := testRequest(t, ts, "GET", parsedURL.Path, &v.want.contentType, nil)
+		assert.Equal(t, 307, resp_get.StatusCode)
 	}
 }
