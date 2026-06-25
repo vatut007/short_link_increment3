7515e16e915e0171f309ceecd8b2b4626d624f0b
Ilya <sov16@ya.ru>
2026-06-25 12:04:33 +0300

    new increment

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -1,9 +1,18 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 func ParseFlags() {
 	flag.StringVar(&ConfigApp.ServerAddress, "a", "localhost:8080", "HTTP server address (e.g. localhost:8888)")
 	flag.StringVar(&ConfigApp.BaseURL, "b", "http://localhost:8080", "Base URL for short links (e.g. http://localhost:8000)")
 	flag.Parse()
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		ConfigApp.ServerAddress = envServerAddress
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		ConfigApp.BaseURL = envBaseURL
+	}
 }
